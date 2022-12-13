95d42a8fd6c1f83bd8e4e265a03cf5524d9e7378
Yjn024 <jiening.yu@outlook.com>
2022-12-13 21:23:14 +0800

    pog

--- src/main/java/com/dm/earth/heatwaves/Heatwaves.java
+++ src/main/java/com/dm/earth/heatwaves/Heatwaves.java
@@ -1,29 +1,30 @@
 package com.dm.earth.heatwaves;
 
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-import org.quiltmc.qsl.command.api.CommandRegistrationCallback;
-
 import com.dm.earth.heatwaves.api.BlockTemperature;
 import com.dm.earth.heatwaves.api.BlockTemperatureKeeper;
 import com.dm.earth.heatwaves.api.BlockTemperatureSource;
 import com.dm.earth.heatwaves.api.TemperatureFactor;
 import com.dm.earth.heatwaves.impl.BiomeTemperatureFactor;
 import com.dm.earth.heatwaves.impl.BlockTemperatureFactor;
+
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.command.api.CommandRegistrationCallback;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.command.CommandBuildContext;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 
-import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
-import net.minecraft.block.Blocks;
-import net.minecraft.command.CommandBuildContext;
-import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
-import net.minecraft.text.Text;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
-
+@SuppressWarnings("UnstableApiUsage")
 public class Heatwaves implements ModInitializer, CommandRegistrationCallback {
 	@Override
-	@SuppressWarnings("UnstableApiUsage")
 	public void onInitialize(ModContainer mod) {
 		TemperatureFactor.register(new BlockTemperatureFactor());
 		TemperatureFactor.register(new BiomeTemperatureFactor());
@@ -32,15 +33,10 @@ public class Heatwaves implements ModInitializer, CommandRegistrationCallback {
 	}
 
 	@Override
-	public void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher, CommandBuildContext buildContext,
-			RegistrationEnvironment environment) {
-		dispatcher.register(
-				CommandManager.literal("heatwaves").then(CommandManager.literal("getTemperature").executes(ctx -> {
-					ctx.getSource().sendFeedback(
-							Text.of(String.valueOf(BlockTemperature.getTemperature(ctx.getSource().getWorld(),
-									ctx.getSource().getPlayer().getBlockPos(), true))),
-							false);
-					return Command.SINGLE_SUCCESS;
-				})));
+	public void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher, CommandBuildContext buildContext, RegistrationEnvironment environment) {
+		dispatcher.register(CommandManager.literal("heatwaves").then(CommandManager.literal("getTemperature").executes(ctx -> {
+			ctx.getSource().sendFeedback(Text.of(String.valueOf(BlockTemperature.getTemperature(ctx.getSource().getWorld(), ctx.getSource().getPlayer().getBlockPos(), true) - FluidConstants.WATER_TEMPERATURE)), false);
+			return Command.SINGLE_SUCCESS;
+		})));
 	}
 }
